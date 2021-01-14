48a9a80b135599512301c37491e227ff31a030af
qy <wangjqyet@163.com>
2021-01-14 17:11:41 +0800

    first commit test!

---
+++ skywalkingDemo/src/main/java/service/serviceImp/skyserviceImp.java
@@ -0,0 +1,14 @@
+package service.serviceImp;
+
+import service.skywalkService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class skyserviceImp implements skywalkService {
+    List<Integer> findAll =  new ArrayList();
+
+    public static void main(String[] args) {
+        System.out.println("Skywalking running test");
+    }
+}
